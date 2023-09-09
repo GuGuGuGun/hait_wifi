b8ba0f6e8b72cb5527ef78845e9993a8c8ca46db
GuGuGuGun <107943071+GuGuGuGun@users.noreply.github.com>
2023-09-09 21:24:22 +0800

    Update hait.py
    优化逻辑，更新post

--- hait.py
+++ hait.py
@@ -7,18 +7,26 @@ import winreg
 import pywifi
 from pywifi import const
 import comtypes
-#开机自启及取消自启
-def add_to_startup(name,file_path=""):
-	#By IvanHanloth
+
+
+# 开机自启及取消自启
+def add_to_startup(name, file_path=""):
+    # By IvanHanloth
     if file_path == "":
         file_path = os.path.realpath(sys.argv[0])
-    auth="IvanHanloth"
-    key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\Microsoft\Windows\CurrentVersion\Run",winreg.KEY_SET_VALUE, winreg.KEY_ALL_ACCESS|winreg.KEY_WRITE|winreg.KEY_CREATE_SUB_KEY)#By IvanHanloth
+    auth = "IvanHanloth"
+    key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\Microsoft\Windows\CurrentVersion\Run",
+                         winreg.KEY_SET_VALUE,
+                         winreg.KEY_ALL_ACCESS | winreg.KEY_WRITE | winreg.KEY_CREATE_SUB_KEY)  # By IvanHanloth
     winreg.SetValueEx(key, name, 0, winreg.REG_SZ, file_path)
     winreg.CloseKey(key)
+
+
 def remove_from_startup(name):
-    auth="IvanHanloth"
-    key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\Microsoft\Windows\CurrentVersion\Run", winreg.KEY_SET_VALUE, winreg.KEY_ALL_ACCESS|winreg.KEY_WRITE|winreg.KEY_CREATE_SUB_KEY)#By IvanHanloth
+    auth = "IvanHanloth"
+    key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\Microsoft\Windows\CurrentVersion\Run",
+                         winreg.KEY_SET_VALUE,
+                         winreg.KEY_ALL_ACCESS | winreg.KEY_WRITE | winreg.KEY_CREATE_SUB_KEY)  # By IvanHanloth
     try:
         winreg.DeleteValue(key, name)
     except FileNotFoundError:
@@ -26,6 +34,8 @@ def remove_from_startup(name):
     else:
         print(f"{name} removed from startup.")
     winreg.CloseKey(key)
+
+
 def get_host_ip():
     """
     查询本机ip地址
@@ -51,8 +61,7 @@ try:
     userPwd = s[1]
     operator = s[2]
     iftrueopen = s[3]
-    print(f'{userId}  {userPwd}  {operator}  {iftrueopen}')
-    if(iftrueopen == 'false'):
+    if (iftrueopen == 'false'):
         print(f'若需开机自启则删除{path}配置文件重新选择')
         if (iftrueopen == 'true'):
             print(f'若需关闭开机自启则删除{path}配置文件重新选择')
@@ -66,10 +75,10 @@ except FileNotFoundError:
     iftrueopen = 'case'
     while True:
         cs = input(
-                   '1.移动\n'
-                   '2.联通\n'
-                   '3.电信\n'
-        '选择通信商(输入数字)：\n')
+            '1.移动\n'
+            '2.联通\n'
+            '3.电信\n'
+            '选择通信商(输入数字)：\n')
         match cs:
             case '1':
                 operator = '@gxyyd'
@@ -106,74 +115,37 @@ except FileNotFoundError:
 
 hostIP = get_host_ip()
 userName = userId + operator
-url = 'http://211.69.15.33:9999/portalAuthAction.do'
+url = f'http://211.69.15.10:6060/quickauth.do?'
 # headers加上payload构成post请求
+
 header = {
-    'Host': '211.69.15.33:9999',
-    'Connection': 'keep-alive',
-    'Content-Length': '644',
-    'Cache-Control': 'max-age=0',
-    'Origin': 'http://211.69.15.33:9999',
-    'Upgrade-Insecure-Requests': '1',
-    'DNT': '1',
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36 Edg/89.0.774.63',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'Referer': f'http://211.69.15.33:9999/portalReceiveAction.do?wlanuserip={hostIP}&wlanacname=HAIT-SR8808',
+    'Accept': 'application/json, text/javascript, */*; q=0.01',
     'Accept-Encoding': 'gzip, deflate',
     'Accept-Language': 'zh-CN,zh;q=0.9',
-    'Cookie': 'JSESSIONID=33529292B95C09300EE439374AAD0DBC.worker3'
+    'Connection': 'keep-alive',
+    'Cookie': 'b-user-id=977405d8-0e5e-8a72-ad4d-29a4a54ce195; macAuth=',
+    'Host': '211.69.15.10:6060',
+    'Referer': f'http://211.69.15.10:6060/portalReceiveAction.do?wlanuserip={hostIP}&wlanacname=HAIT-SR8808',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
+    'X-Requested-With': 'XMLHttpRequest'
+}
+data = {
+    'userid': userName,
+    'passwd': userPwd,
+    'wlanuserip': hostIP,
+    'wlanacname': 'HAIT-SR8808',
+    'wlanacIp': '172.21.8.73',
+    'ssid': '',
+    'vlan': '',
+    'mac': '',
+    'version': '0',
+    'portalpageid': '21',
+    'portaltype': '0',
+    'hostname': '',
+    'bindCtrlId': '',
 }
 
-data = {'wlanuserip': hostIP,
-        'wlanacname': 'HAIT-SR8808',
-        'chal_id': '',
-        'chal_vector': '',
-        'auth_type': 'PAP',
-        'seq_id': '',
-        'req_id': '',
-        'wlanacIp': '172.21.8.73',
-        'ssid': '',
-        'vlan': '',
-        'mac': '',
-        'message': '',
-        'bank_acct': '',
-        'isCookies': '',
-        'version': '0',
-        'authkey': '88----89',
-        'url': '',
-        'usertime': '0',
-        'listpasscode': '0',
-        'listgetpass': '0',
-        'getpasstype': '0',
-        'randstr': '5099',
-        'domain': '',
-        'isRadiusProxy': 'true',
-        'usertype': '0',
-        'isHaveNotice': '0',
-        'times': '12',
-        'weizhi': '0',
-        'smsid': '',
-        'freeuser': '',
-        'freepasswd': '',
-        'listwxauth': '0',
-        'templatetype': '1',
-        'tname': 'gxy_pc_portal',
-        'logintype': '0',
-        'act': '',
-        'is189': 'false',
-        'terminalType': '',
-        'checkterminal': 'true',
-        'portalpageid': '23',
-        'listfreeauth': '0',
-        'viewlogin': '1',
-        'userid': userName,
-        'authGroupId': '',
-        'smsoperatorsflat': '',
-        'useridtemp': userName,
-        'passwd': userPwd,
-        'operator': operator,
-        }
+
 def connect():
     """进行认证\n
     认证成功->True\n
@@ -182,20 +154,27 @@ def connect():
     req = requests.post(url, headers=header, data=data)
     req.encoding = req.apparent_encoding
     print(req.text)
-    if '<title>校园黄页</title>' in req.text:
+    if '"message":null' in req.text:
         return True
     elif url in req.url:
         print('3s后重新连接')
         time.sleep(3)
         connect()
 
+    elif '"message":"AC999"' in req.text:
+        print("当前已连接网络,无需再次连接")
+        return True
     else:
         print(req.text)
         return False
     # 测试ping
+
+
 def ping_test(url='www.baidu.com', n=1):
     ret = os.system("ping baidu.com -n 1")
     return True if ret == 0 else False
+
+
 def wifi_connect_status():
     wifi = pywifi.PyWiFi()
     iface = wifi.interfaces()[0]  # acquire the first Wlan card,maybe not
@@ -206,6 +185,8 @@ def wifi_connect_status():
         print("wifi 未连接!")
 
     return 0
+
+
 if __name__ == '__main__':
     print('当前ip地址为:', get_host_ip())
     i = 1  # 若未连接进行五次循环
