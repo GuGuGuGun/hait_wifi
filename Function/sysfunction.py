611942268c8eab09e7d704fbc172ef2fc0614c1c
GuGuGuGun <107943071+GuGuGuGun@users.noreply.github.com>
2023-09-10 16:54:34 +0800

    Add files via upload

---
+++ Function/sysfunction.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import winreg
+
+
+class SysFunction:
+    def __init__(self,name,file_path=""):
+        self.name = name
+        self.file_path = file_path
+    def add_to_startup(self, file_path=""):
+        if file_path == "":
+            file_path = os.path.realpath(sys.argv[0])
+        auth = "IvanHanloth"
+        key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\Microsoft\Windows\CurrentVersion\Run",
+                             winreg.KEY_SET_VALUE,
+                             winreg.KEY_ALL_ACCESS | winreg.KEY_WRITE | winreg.KEY_CREATE_SUB_KEY)
+        winreg.SetValueEx(key, self.name, 0, winreg.REG_SZ, file_path)
+        winreg.CloseKey(key)
+
+    def remove_from_startup(name):
+        auth = "IvanHanloth"
+        key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\Microsoft\Windows\CurrentVersion\Run",
+                             winreg.KEY_SET_VALUE,
+                             winreg.KEY_ALL_ACCESS | winreg.KEY_WRITE | winreg.KEY_CREATE_SUB_KEY)
+        try:
+            winreg.DeleteValue(key, name)
+        except FileNotFoundError:
+            print(f"{name} not found in startup.")
+        else:
+            print(f"{name} removed from startup.")
+        winreg.CloseKey(key)
