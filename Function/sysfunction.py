551afb68ff6e94a86920f7bfcc05437ab1bb8541
GuGuGuGun <107943071+GuGuGuGun@users.noreply.github.com>
2023-09-10 16:58:57 +0800

    Update sysfunction.py

--- Function/sysfunction.py
+++ Function/sysfunction.py
@@ -17,15 +17,15 @@ class SysFunction:
         winreg.SetValueEx(key, self.name, 0, winreg.REG_SZ, file_path)
         winreg.CloseKey(key)
 
-    def remove_from_startup(name):
+    def remove_from_startup((self):
         auth = "IvanHanloth"
         key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\Microsoft\Windows\CurrentVersion\Run",
                              winreg.KEY_SET_VALUE,
                              winreg.KEY_ALL_ACCESS | winreg.KEY_WRITE | winreg.KEY_CREATE_SUB_KEY)
         try:
-            winreg.DeleteValue(key, name)
+            winreg.DeleteValue(key, self.name)
         except FileNotFoundError:
-            print(f"{name} not found in startup.")
+            print(f"{self.name} not found in startup.")
         else:
-            print(f"{name} removed from startup.")
+            print(f"{self.name} removed from startup.")
         winreg.CloseKey(key)
