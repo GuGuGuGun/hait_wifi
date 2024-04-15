83e684c73eae5dc9f649f0bd642f649bc0f2dbf4
GuGuGuGun <107943071+GuGuGuGun@users.noreply.github.com>
2024-04-15 18:29:54 +0800

    Update hait.py

--- hait.py
+++ hait.py
@@ -1,3 +1,4 @@
+import ctypes
 import time
 from Function import webfunction
 from Function import wififunction
@@ -6,18 +7,25 @@ import os
 import sys
 
 filename = 'user_hait_setting'
-desktop_path = ".\\"  # 新创建的txt文件的存放路径
+desktop_path = "C:\\"  # 新创建的txt文件的存放路径
 path = desktop_path + filename + '.txt'
 is_new = False
-def add_to_startup(name,file_path=""):
+
+
+def add_to_startup(name, file_path=""):
     if file_path == "":
         file_path = os.path.realpath(sys.argv[0])
-    key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\Microsoft\Windows\CurrentVersion\Run",winreg.KEY_SET_VALUE, winreg.KEY_ALL_ACCESS|winreg.KEY_WRITE|winreg.KEY_CREATE_SUB_KEY)#By IvanHanloth
+    key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\Microsoft\Windows\CurrentVersion\Run",
+                         winreg.KEY_SET_VALUE,
+                         winreg.KEY_ALL_ACCESS | winreg.KEY_WRITE | winreg.KEY_CREATE_SUB_KEY)  # By IvanHanloth
     winreg.SetValueEx(key, name, 0, winreg.REG_SZ, file_path)
     winreg.CloseKey(key)
 
+
 def remove_from_startup(name):
-    key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\Microsoft\Windows\CurrentVersion\Run", winreg.KEY_SET_VALUE, winreg.KEY_ALL_ACCESS|winreg.KEY_WRITE|winreg.KEY_CREATE_SUB_KEY)#By IvanHanloth
+    key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\Microsoft\Windows\CurrentVersion\Run",
+                         winreg.KEY_SET_VALUE,
+                         winreg.KEY_ALL_ACCESS | winreg.KEY_WRITE | winreg.KEY_CREATE_SUB_KEY)  # By IvanHanloth
     try:
         winreg.DeleteValue(key, name)
     except FileNotFoundError:
@@ -25,6 +33,15 @@ def remove_from_startup(name):
     else:
         print(f"{name} removed from startup.")
     winreg.CloseKey(key)
+
+
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+
 try:
     with open(path) as f:
         s = f.read().strip().split()
@@ -36,53 +53,58 @@ try:
         print(f'若需开机自启则删除{path}配置文件重新选择')
         if (iftrueopen == 'true'):
             print(f'若需关闭开机自启则删除{path}配置文件重新选择')
+
 except FileNotFoundError:
-    is_new = True
-    print('当前为第一次使用')
-    print(f'完成初始化之后，信息将被写入{path}中')
-    userId = input('请输入学号：')
-    userPwd = input('请输入密码：')
-    operator = '@'
-    iftrueopen = 'case'
-    while True:
-        cs = input(
-            '1.移动\n'
-            '2.联通\n'
-            '3.电信\n'
-            '选择通信商(输入数字)：\n')
-        match cs:
-            case '1':
-                operator = '@gxyyd'
-                break
-            case '2':
-                operator = '@gxylt'
-                break
-            case '3':
-                operator = '@gxydx'
-                break
-            case _:
-                print('输入值有误，请重新输入：')
-    while True:
-        choice = input(
-            '是否开机自启，请选择\n'
-            '1.是\n'
-            '2.否（若已开启开机自启则选此选项删除）\n'
-            '请输入数字：\n')
-        match choice:
-            case '1':
-                iftrueopen = 'true'
-                add_to_startup("hait_wifi")
-                break
-            case '2':
-                iftrueopen = 'false'
-                remove_from_startup("hait_wifi")
-                break
-            case _:
-                print('输入值有误，请重新输入：')
-    with open(path, 'wt') as w:
-        w.write(f'{userId}\n{userPwd}\n{operator}\n{iftrueopen}')
-        w.flush()
-        print('初始化完成！')
+    if is_admin():
+        is_new = True
+        print('当前为第一次使用')
+        print(f'完成初始化之后，信息将被写入{path}中')
+        userId = input('请输入学号：')
+        userPwd = input('请输入密码：')
+        operator = '@'
+        iftrueopen = 'case'
+        while True:
+            cs = input(
+                '1.移动\n'
+                '2.联通\n'
+                '3.电信\n'
+                '选择通信商(输入数字)：\n')
+            match cs:
+                case '1':
+                    operator = '@gxyyd'
+                    break
+                case '2':
+                    operator = '@gxylt'
+                    break
+                case '3':
+                    operator = '@gxydx'
+                    break
+                case _:
+                    print('输入值有误，请重新输入：')
+        while True:
+            choice = input(
+                '是否开机自启，请选择\n'
+                '1.是\n'
+                '2.否（若已开启开机自启则选此选项删除）\n'
+                '请输入数字：\n')
+            match choice:
+                case '1':
+                    iftrueopen = 'true'
+                    add_to_startup("hait_wifi")
+                    break
+                case '2':
+                    iftrueopen = 'false'
+                    remove_from_startup("hait_wifi")
+                    break
+                case _:
+                    print('输入值有误，请重新输入：')
+        with open(path, 'wt') as w:
+            w.write(f'{userId}\n{userPwd}\n{operator}\n{iftrueopen}')
+            w.flush()
+            print('初始化完成！')
+    if not is_admin():
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
+        sys.exit()
 
 hostIP = wififunction.get_host_ip()
 userName = userId + operator
@@ -116,7 +138,6 @@ data = {
     'bindCtrlId': '',
 }
 
-
 if __name__ == '__main__':
     print('当前ip地址为:', wififunction.get_host_ip())
     i = 1  # 若未连接进行五次循环
@@ -124,16 +145,17 @@ if __name__ == '__main__':
         if wififunction.WifiFunction.wifi_connect_status(1):  # wifi为连接状态，开始认证
             print('Wifi已连接，正在尝试认证...')
             print('正在进行第%d次连接...' % i)
-            if webfunction.WebPost(url,header, data).connect():  # 进行认证
+            if webfunction.WebPost(url, header, data).connect():  # 进行认证
                 print('认证成功...')
                 time.sleep(1)
                 wififunction.WifiFunction.ping_test(1)
                 time.sleep(1)
                 print('认证完成，程序将在15秒后退出')
-                time.sleep(3)
+                time.sleep(15)
                 break
             else:
-                print('认证失败，请查看返回信息：')
+                print('认证失败，请查看返回信息,3秒后自动重连')
+                time.sleep(3)
         else:
             print('WiFi未连接，请手动连接WiFi后，按回车键重新尝试连接')
             temp = input()
